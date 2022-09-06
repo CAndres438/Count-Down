de012134fb6e97033f3c995713624804d2bbae1e
Carlos Andres Ortiz <candresortiz123@gmail.com>
2022-09-06 00:16:23 -0500

    Update Footer.js
    Segunda modificación de extensiones servidor.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <FooterStyled>
             <SocialIconsStyled>
-            <span><img src="https://res.cloudinary.com/daalu/image/upload/v1650271669/Recursos_Dark/icon-facebook_waa2ve.svg" alt="facebook"/> <img src="https://res.cloudinary.com/daalu/image/upload/v1650271669/Recursos_Dark/icon-pinterest_bqs2cr.svg" alt="pinterest"/> <img src="https://res.cloudinary.com/daalu/image/upload/v1650271669/Recursos_Dark/icon-instagram_k25cb6.svg" alt="instagram"/></span>
+            <span><img src="https://res.cloudinary.com/cortiz/image/upload/v1650271669/Recursos_Dark/icon-facebook_waa2ve.svg" alt="facebook"/> <img src="https://res.cloudinary.com/cortiz/image/upload/v1650271669/Recursos_Dark/icon-pinterest_bqs2cr.svg" alt="pinterest"/> <img src="https://res.cloudinary.com/cortiz/image/upload/v1650271669/Recursos_Dark/icon-instagram_k25cb6.svg" alt="instagram"/></span>
             
             </SocialIconsStyled>
         </FooterStyled>
